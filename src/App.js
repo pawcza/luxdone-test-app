7c6695fbe4fb7650fac2b88e65c99a660fb93bfc
pawcza <hidd95@gmail.com>
2022-06-21 17:35:15 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Chart from "./components/Chart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return <Container>
+            <Chart/>
+        </Container>
 }
 
 export default App;
+
+export const Container = styled.div`
+    margin: 0 auto;
+    width: 100%;
+    max-width: 1280px;
+    position: relative;
+    overflow-x: hidden;
+`;
